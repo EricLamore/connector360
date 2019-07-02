958e05d67bef5f89ca8e7fe99e32813f480becd5
Fayçal Benaziz <faycal.1393@gmail.com>
2019-07-02 19:51:30 +0200

    Sass variable used for details button background color

--- src/features/global-reporting/dashboard/components/details-button/details-button.component.ts
+++ src/features/global-reporting/dashboard/components/details-button/details-button.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
 	selector: 'app-details-button',
-	templateUrl: './details-button.component.html'
+	templateUrl: './details-button.component.html',
+	styleUrls: ['./details-button.component.scss']
 })
 export class DetailsButtonComponent {
 	@Input() public routerLink: string[];
