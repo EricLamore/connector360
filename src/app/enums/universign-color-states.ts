07426216abff068bc51734ffbaab9101fb5b14f6
Fayçal Benaziz <faycal.1393@gmail.com>
2019-07-02 15:44:10 +0200

    Projects Timeline fake service implemented

--- src/app/enums/universign-color-states.ts
+++ src/app/enums/universign-color-states.ts
@@ -1,7 +1,8 @@
 enum UniversignColorStates {
 	OK = '#C7D100',
-	INFO = '#00467f',
+	INFO = '#00467F',
 	WARNING = '#E85911',
+	DANGEROUS = '#B5007C',
 	OTHERS = '#F4C200'
 }
 export default UniversignColorStates;
