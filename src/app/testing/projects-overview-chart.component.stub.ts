9ac17752cedadc907e8c50c64c33f377617914e8
Fayçal Benaziz <faycal.1393@gmail.com>
2019-07-02 11:31:45 +0200

    Stub fixed for project overview.

--- src/app/testing/projects-overview-chart.component.stub.ts
+++ src/app/testing/projects-overview-chart.component.stub.ts
@@ -1,10 +1,7 @@
-import { Component, Input } from '@angular/core';
-import { ChartDataSets } from 'chart.js';
+import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-projects-overview-chart',
 	template: ''
 })
-export class ProjectsOverviewChartStubComponent {
-	@Input() public projectsOverviewChartData: ChartDataSets[];
-}
+export class ProjectsOverviewChartStubComponent {}
